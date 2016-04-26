0033d02d70ece4a336c03596a2101dcdbfe1a58d
ReneeVL85 <renee.v.leijen@gmail.com>
2016-04-26 20:12:49 +0200

    added controller stuff plus bootstrap

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,45 @@ class ArtistsController < ApplicationController
     @artists = Artist.order(name: :asc)
   end
 
-  def show
-    @artist = Artist.find( params[:id] )
+  def create
+    @artist = Artist.new(artist_params)
+      if artist.save
+        rederict to artists_path
+      else
+        render "new"
+      end
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+
+  def update
+    @artist = Artist.find(params[:id])
+      if @artist.update_attributes(artist_params)
+        redirect_to @artist
+      else
+        render "edit"
+      end
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+
+    @artist.destroy
+
+    redirect_to artists_path
+  end
+
+  private
+
+     def artist_params
+       params.require( :artist ).permit( :name, :country, :bio )
+     end
+
 end
